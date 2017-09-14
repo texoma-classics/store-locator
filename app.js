b2d54acfd173d2c1604056ded10422c0f2ff60b5
Texoma Classics <31775843+texoma-classics@users.noreply.github.com>
2017-09-14 11:13:47 -0500

    add files via upload

---
+++ app.js
@@ -0,0 +1,143 @@
+const mapStyle = [
+  {
+    "featureType": "administrative",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 33
+      }
+    ]
+  },
+  {
+    "featureType": "landscape",
+    "elementType": "all",
+    "stylers": [
+      {
+        "color": "#f2e5d4"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5dac6"
+      }
+    ]
+  },
+  {
+    "featureType": "poi.park",
+    "elementType": "labels",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road",
+    "elementType": "all",
+    "stylers": [
+      {
+        "lightness": 20
+      }
+    ]
+  },
+  {
+    "featureType": "road.highway",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#c5c6c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.arterial",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#e4d7c6"
+      }
+    ]
+  },
+  {
+    "featureType": "road.local",
+    "elementType": "geometry",
+    "stylers": [
+      {
+        "color": "#fbfaf7"
+      }
+    ]
+  },
+  {
+    "featureType": "water",
+    "elementType": "all",
+    "stylers": [
+      {
+        "visibility": "on"
+      },
+      {
+        "color": "#acbcc9"
+      }
+    ]
+  }
+];
+
+function initMap() {
+
+  // Create the map.
+  const map = new google.maps.Map(document.getElementsByClassName('map')[0], {
+    zoom: 5,
+    center: {lat: 38.0800341, lng: -98.6063015},
+    styles: mapStyle
+  });
+
+  // Load the stores GeoJSON onto the map.
+  map.data.loadGeoJson('stores.json');
+
+  // Define the custom marker icons, using the store's "category".
+  map.data.setStyle(feature => {
+    return {
+      icon: {
+        url: `img/icon_${feature.getProperty('category')}.png`,
+        scaledSize: new google.maps.Size(64, 64)
+      }
+    };
+  });
+
+  const apiKey = 'AIzaSyAaPJmeasIKqNQqA65KpLRZXp2vib41Mqc';
+  const infoWindow = new google.maps.InfoWindow();
+
+  // Show the information for a store when its marker is clicked.
+  map.data.addListener('click', event => {
+
+    let category = event.feature.getProperty('category');
+    let name = event.feature.getProperty('name');
+    let description = event.feature.getProperty('description');
+    let hours = event.feature.getProperty('hours');
+    let phone = event.feature.getProperty('phone');
+    let position = event.feature.getGeometry().get();
+    let content = `
+      <img style="float:left; width:200px; margin-top:30px" src="img/logo_${category}.png">
+      <div style="margin-left:220px; margin-bottom:20px;">
+        <h2>${name}</h2><p>${description}</p>
+        <p><b>Open:</b> ${hours}<br/><b>Phone:</b> ${phone}</p>
+        <p><img src="https://maps.googleapis.com/maps/api/streetview?size=350x120&location=${position.lat()},${position.lng()}&key=${apiKey}"></p>
+      </div>
+    `;
+
+    infoWindow.setContent(content);
+    infoWindow.setPosition(position);
+    infoWindow.setOptions({pixelOffset: new google.maps.Size(0, -30)});
+    infoWindow.open(map);
+  });
+
+}
